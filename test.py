cd777769ad90db8699442a001327bd77645f4247
Ubuntu <ubuntu@ip-172-31-4-149.ap-south-1.compute.internal>
2026-03-31 14:40:33 +0000

    DevOps CI/CD Project

---
+++ test.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("file:///home/ubuntu/devops-project/index.html")
+text = driver.find_element(By.TAG_NAME,"h1").text
+assert "DevOps" in text
+print("Test Passed")
+driver.quit()
